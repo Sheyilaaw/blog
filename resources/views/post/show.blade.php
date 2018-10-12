3e31a1e98d7e4ac40196dd9040ae879bcd88de62
Sheyilaaw <sheyilaaw98@gmail.com>
2018-10-12 21:01:41 +0100

    Refactored code to fetch post array using associative index notation

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -5,16 +5,14 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card-body">
-                        @foreach($posts as $post)
-                            <div class="col-md-12">
-                                <div class="jumbotron">
-                                    <h2 class="display-3">{{$post->title}}</h2>
-                                    <p class="lead">
-                                        {{$post->body}}
-                                    </p>
-                                </div>
-                            </div>
-                        @endforeach
+                    <div class="col-md-12">
+                        <div class="jumbotron">
+                            <h2 class="display-3">{{$post['title']}}</h2>
+                            <p class="lead">
+                                {{$post['body']}}
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
